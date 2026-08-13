ce9b949db7c0f3a7fd17f4b84b2c06810740909a
Ivan Lebedev <ivan_lbd@mail.ru>
2026-08-13 10:57:42 +0300

    add user entity method

--- app/domain/entities/user.py
+++ app/domain/entities/user.py
@@ -28,3 +28,24 @@ class User:
             raise InvalidUserError('User email is invalid.')
         if not self.hashed_password or not self.hashed_password.strip():
             raise InvalidUserError('User hashed password cannot be empty.')
+
+    def is_admin(self) -> bool:
+        return self.role is UserRole.ADMIN
+
+    def is_author(self) -> bool:
+        return self.role is UserRole.AUTHOR
+
+    def is_student(self) -> bool:
+        return self.role is UserRole.STUDENT
+
+    def can_manage_platform(self) -> bool:
+        return self.is_admin()
+
+    def can_manage_learning_content(self) -> bool:
+        return self.role in {UserRole.ADMIN, UserRole.AUTHOR}
+
+    def can_take_learning_activities(self) -> bool:
+        return self.is_student()
+
+    def can_manage_content(self) -> bool:
+        return self.can_manage_platform()
