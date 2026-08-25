1bed1b5f968370084bf87c58cf2a755fcff4d0db
Ivan Lebedev <ivan_lbd@mail.ru>
2026-08-25 17:14:18 +0300

    add UoW, update course write use-cases

---
+++ app/application/interfaces/unit_of_work.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from types import TracebackType
+
+from .repositories import (
+    CourseRepository,
+    LectureRepository,
+    ModuleRepository,
+    SectionRepository,
+)
+
+
+class UnitOfWork(ABC):
+    courses: CourseRepository
+    modules: ModuleRepository
+    sections: SectionRepository
+    lectures: LectureRepository
+
+    @abstractmethod
+    async def __aenter__(self) -> 'UnitOfWork':
+        raise NotImplementedError
+
+    @abstractmethod
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        tb: TracebackType | None,
+    ) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def commit(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def rollback(self) -> None:
+        raise NotImplementedError
